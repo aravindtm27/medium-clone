e9438dc99c41ea0b521ca87aa31cf1bdf1a5ba71
aravindtm27 <145952967+aravindtm27@users.noreply.github.com>
2024-05-30 05:55:24 +0530

    deploy bug fix

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -2,7 +2,7 @@ import { BlogPage } from "../components/Blogpage";
 import { useBlog } from "../hooks";
 import { useParams } from "react-router-dom";
 
-export const Blog =() => {
+export const Blog = () => {
   const { id } = useParams();
   const {loading, blog} = useBlog({id: id || ""});
 
@@ -14,8 +14,7 @@ export const Blog =() => {
 
   return (
     <div>
-      // @ts-ignore
-      <BlogPage blog={blog}/>
+      <BlogPage blog={blog} />
     </div>
   )
 }
